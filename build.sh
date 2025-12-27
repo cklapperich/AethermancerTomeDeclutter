7e270c78336881b58f6c4804da25506241ef4e21
Chris Klapperich <cklapperichmn@gmail.com>
2025-12-27 12:25:17 -0600

    license and build.sh updates

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 GAME_DIR="$HOME/.steam/debian-installation/steamapps/common/Aethermancer"
 PLUGIN_DIR="$GAME_DIR/BepInEx/plugins"
 DLL_NAME="AethermancerTomeDeclutter.dll"
+ZIP_NAME="AethermancerTomeDeclutter.zip"
 
 echo "Building..."
 dotnet build -c Release
@@ -12,4 +13,13 @@ echo "Copying to $PLUGIN_DIR"
 mkdir -p "$PLUGIN_DIR"
 cp "bin/Release/net472/$DLL_NAME" "$PLUGIN_DIR/"
 
-echo "Done! Installed to $PLUGIN_DIR/$DLL_NAME"
+echo "Creating release zip..."
+TEMP_DIR=$(mktemp -d)
+mkdir -p "$TEMP_DIR/BepInEx/plugins"
+cp "bin/Release/net472/$DLL_NAME" "$TEMP_DIR/BepInEx/plugins/"
+(cd "$TEMP_DIR" && zip -r "$OLDPWD/$ZIP_NAME" BepInEx)
+rm -rf "$TEMP_DIR"
+
+echo "Done!"
+echo "  Installed to: $PLUGIN_DIR/$DLL_NAME"
+echo "  Release zip:  $ZIP_NAME"
